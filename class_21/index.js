eaa060e5808b7543aaaf543ec359c2fd46754e57
HrithikMittal <adhikanshmittal@gmail.com>
2025-02-18 12:59:52 +0530

    add class 21 notes

---
+++ class_21/index.js
@@ -0,0 +1,17 @@
+console.log("Question 1: Reverse a string");
+let word = "Hello World";
+// dlroW olleH
+
+console.log("Question 2: Reverse words of a string");
+let description = "Hello World THis is me";
+// me is This World Hello
+
+console.log("Question 3: Find the Most Frequent Word in a Paragraph");
+description = "apple banana apple orange banana apple";
+// apple
+
+console.log("Question 4: Implement Run-Length Encoding");
+description = "aaabbcddd";
+// a3b2c1d3
+("aaabbcdddaaa");
+("a3b2c1d3a3");
